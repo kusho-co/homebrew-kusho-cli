5ad96295ae0f35d446e716c4f55a693dea633dba
sahil-kusho <sahil.jha@kusho.co>
2024-11-06 19:57:29 +0530

    updated formula

--- Formula/kusho-cli.rb
+++ Formula/kusho-cli.rb
@@ -1,8 +1,8 @@
 class KushoCli < Formula
   desc "Kusho CLI - a bash script packaged for Homebrew"
   homepage "https://github.com/kusho-co/kusho-cli-brew"
-  url "https://github.com/YOUR-GITHUB-USERNAME/REPO-NAME/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "<SHA256-checksum-of-the-tar-file>"
+  url "https://github.com/kusho-co/kusho-cli-brew/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "78d1dc384913f0b3bafc13a8895284e1380832bf70c6346c0bfabf7f2f0aece8"
   version "1.0.0"
 
   depends_on "uuidgen" # Ensures uuidgen is available
