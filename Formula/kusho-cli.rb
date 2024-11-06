a11cd2648e9e2a04c4bf166d0eeb4a658ce61a18
sahil-kusho <sahil.jha@kusho.co>
2024-11-06 20:07:00 +0530

    Update checksum for v1.0.0

--- Formula/kusho-cli.rb
+++ Formula/kusho-cli.rb
@@ -2,7 +2,7 @@ class KushoCli < Formula
   desc "Kusho CLI - a bash script packaged for Homebrew"
   homepage "https://github.com/kusho-co/kusho-cli-brew"
   url "https://github.com/kusho-co/kusho-cli-brew/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "78d1dc384913f0b3bafc13a8895284e1380832bf70c6346c0bfabf7f2f0aece8"
+  sha256 "dec6d4bd031c8b387731bb89f8ad4f245f8ffad247372b977d8cfa94a51478a9"
   version "1.0.0"
 
 
