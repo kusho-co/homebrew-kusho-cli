4a5017c712938e6e2f266b22e59911d677bb439e
sahil-kusho <sahil.jha@kusho.co>
2024-11-07 13:05:11 +0530

    Add formula for v1.0.0

--- Formula/kusho-cli.rb
+++ Formula/kusho-cli.rb
@@ -2,7 +2,7 @@ class KushoCli < Formula
   desc "Kusho CLI - a bash script packaged for Homebrew"
   homepage "https://github.com/kusho-co/kusho-cli-brew"
   url "https://github.com/kusho-co/kusho-cli-brew/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "dec6d4bd031c8b387731bb89f8ad4f245f8ffad247372b977d8cfa94a51478a9"
+  sha256 "d8c837bacdc9f5bf678462aefd9c09655c8d0cb24e9f1898b2ad527468fdfdb5"
   version "1.0.0"
 
   def install
