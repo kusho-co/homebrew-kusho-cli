cbd310ba7128be591ebe5eb7108a40480ef17027
sahil-kusho <sahil.jha@kusho.co>
2024-11-08 13:11:54 +0530

    updated correct formula

--- Formula/kusho-cli.rb
+++ Formula/kusho-cli.rb
@@ -8,7 +8,7 @@ class KushoCli < Formula
   def install
     # Instead of installing directly to bin, we'll create a libexec directory
     # This is a common pattern in Homebrew for scripts
-    libexec.install "kusho-cli.sh" => "kusho-cli"
+    libexec.install "bin/kusho-cli.sh" => "kusho-cli"
     
     # Make the libexec script executable
     chmod 0755, libexec/"kusho-cli"
